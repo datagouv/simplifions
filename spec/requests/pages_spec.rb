046760bd7bf5249a6621e17150ab3aae154799f2
Thomas COMES <thomas.comes.tech@gmail.com>
2026-09-01 15:45:29 +0200

    Point the home explorer at the catalogue lists
    Its two selects now submit target-users and fournisseurs-de-service to
    /demarches with the same values as the facet tags, so the published explorer
    links keep working.

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -11,6 +11,23 @@ RSpec.describe 'Pages' do
     end
   end
 
+  describe 'explorateur de la page d’accueil' do
+    it 'envoie vers /demarches avec les facettes du site' do
+      Vocabulaire.create!(nom: 'Particuliers', slug: 'particuliers', categorie: 'usager')
+
+      get root_path
+
+      expect(response.body).to include('<form class="explorateur" action="/demarches" accept-charset="UTF-8" method="get">')
+      expect(response.body).to include('<select class="fr-select" id="select-target-users" name="target-users">')
+      expect(response.body).to include('<option value="">particuliers, entreprises, associations</option>')
+      expect(response.body).to include('<option value="particuliers">Particuliers</option>')
+      expect(response.body).to include('<select class="fr-select" id="select-fournisseurs" name="fournisseurs-de-service">')
+      expect(response.body).to include('<option value="communes">Communes et groupements de communes</option>')
+      expect(response.body).to include('<button type="submit" class="fr-btn fr-btn--icon-right fr-icon-search-line">')
+      expect(response.body).not_to include('href="#" class="fr-btn fr-btn--icon-right fr-icon-search-line"')
+    end
+  end
+
   describe 'GET /about' do
     it 'renders the about page mirroring simplifions.data.gouv.fr/about' do
       get about_path
